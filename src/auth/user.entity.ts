6cc6185f9f6df0a1fbd7ac09f490ec33f03565ca
Fikasentani <mpilo.dlamini@yahoo.com>
2021-05-17 08:54:37 +0200

    adding logic to prevent duplicate usernames

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,13 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+@Unique(['username']) // checking if username colum is unique, this happens on the db not api
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,8 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { exists } from 'fs';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -11,6 +16,35 @@ export class UserRepository extends Repository<User> {
     user.username = username;
     user.password = password;
 
-    await user.save();
+    try {
+      await user.save();
+    } catch (e) {
+      console.log(e.code);
+      if (e.code === '23505') {
+        //duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
+
+// @EntityRepository(User)
+// export class UserRepository extends Repository<User> {
+//   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+//     const { username, password } = authCredentialsDto;
+
+//     const exists = this.findOne({username})
+//     if (exists){
+//       // throw some error
+//       //this is not so good becuase we query the db twicw
+//     }
+
+//     const user = new User();
+//     user.username = username;
+//     user.password = password;
+
+//     await user.save();
+//   }
+// }
