023e6eb48925c7a5058dc55584337c974ac2b49c
Fikasentani <mpilo.dlamini@yahoo.com>
2021-06-16 16:34:44 +0200

    testing create task

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -13,6 +13,7 @@ const mockUser = { id: 1, username: 'TestUser' };
 const mockTaskRepository = () => ({
   getTasks: jest.fn(),
   findOne: jest.fn(),
+  createTask: jest.fn(),
 });
 
 describe('Task Servce', () => {
@@ -65,6 +66,23 @@ describe('Task Servce', () => {
       expect(tasksService.getTaskById(1, mockUser)).rejects.toThrow(
         NotFoundException,
       );
+    });
+  });
+
+  describe('createTask', () => {
+    it('calls taskRepository.create() and returns the result', async () => {
+      taskRepository.createTask.mockResolvedValue('someTask');
+      expect(taskRepository.createTask).not.toHaveBeenCalled();
+      const createTaskDto = {
+        title: 'Test task',
+        description: 'Test Description',
+      };
+      const result = await tasksService.createTask(createTaskDto, mockUser);
+      expect(taskRepository.createTask).toHaveBeenCalledWith(
+        createTaskDto,
+        mockUser,
+      );
+      expect(result).toEqual('someTask');
     });
   });
 });
